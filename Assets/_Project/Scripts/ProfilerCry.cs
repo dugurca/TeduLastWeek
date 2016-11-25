4634ab0d14d0c1d9b169ac47961c25373daf7e6f
Deniz Ugurca <dugurca@gmail.com>
2016-11-25 22:46:32 +0300

    .

---
+++ Assets/_Project/Scripts/ProfilerCry.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets._Project.Scripts
+{
+    public class ProfilerCry : MonoBehaviour
+    {
+        private Vector3 _v3 = new Vector3(1, 1, 1);
+        void Update ()
+        {
+            Vector3 x = Random.insideUnitSphere;
+            for (int i = 0; i < 50000; i++)
+            {
+                Vector3 v = new Vector3(Random.value, Random.value, Random.value);
+                x += v;
+            }
+            x = Vector3.zero;
+            _v3 = x;
+            _v3 = Vector3.one;
+        }
+    }
+}
